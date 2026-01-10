00588c3d81087b88ab1181e12bdf1938b70c06f7
masdk9 <36520707+masdk9@users.noreply.github.com>
2026-01-10 20:22:24 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-javascript 
+document.querySelectorAll('.bi-heart').forEach(icon => {
+  icon.addEventListener('click', () => {
+    icon.classList.toggle('active');
+  });
+});
+
+function openMenu() {
+  document.getElementById("sideMenu").classList.add("open");
+}
+
+function closeMenu() {
+  document.getElementById("sideMenu").classList.remove("open");
+}
